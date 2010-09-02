0e47793a39448048a6d9be054e641d4008e2c013
Ben Blackburne <bpb@lysozyme.net>
2010-09-02 21:55:51 +0100

    Initial work on Optimisation of parameters

--- src/main/scala/Model.scala
+++ src/main/scala/Model.scala
@@ -3,6 +3,7 @@ import modiphy.math.EnhancedMatrix._
 import modiphy.tree._
 import modiphy.util._
 import scala.collection.LinearSeq
+import modiphy.opt._
 
 
 object Types{
@@ -10,24 +11,21 @@ object Types{
 }
 import Types._
 
-case class ParamName(i:Int)
-sealed abstract class Restriction
-case class LowerBound(d:Double) extends Restriction
-case class UpperBound(d:Double) extends Restriction
-case object SumToOne extends Restriction
-case object LowerTriagonalMatrix extends Restriction
-case class ParamHandle[A](p:ParamName,param:A,restrictions:Seq[Restriction])
-
 trait Model{
+  def paramMap:Map[(ParamName,Int),Any]
+  lazy val params=paramMap.keys.toList
 }
 trait SingleModel extends Model{
+  val subModelIndex=0
   def pi(n:Node):IndexedSeq[Double]
   def apply(e:Edge):Matrix
 }
 trait StdModel extends SingleModel{
+  def pi:IndexedSeq[Double]
+  def mat:Matrix
   val exp:Exp
   def apply(e:Edge)=exp(e.dist)
-  def mat:Matrix
+  lazy val paramMap=Map[(ParamName,Int),Any]((Pi,subModelIndex)->pi,(S,subModelIndex)->mat)
   /*
   def u:Matrix
   def d:Vector
@@ -38,6 +36,7 @@ trait StdMixtureModel extends Model{
   def models:Seq[SingleModel]
   def apply(e:Edge)=models.map{m=>m(e)}
   def priors:IndexedSeq[Double]
+  lazy val paramMap = models.map{_.paramMap}.foldLeft(Map[(ParamName,Int),Any]()){_++_}
 }
 trait Exp{
   def apply(bl:Double,rate:Double=1.0)=expInt(bl*rate)
@@ -80,15 +79,17 @@ object DefaultExpFactory extends ExpFactory{
   def setDefault(fact:ExpFactory){default=fact}
 }
 
-class BasicLikelihoodModel(piValues:IndexedSeq[Double],s:LinearSeq[LinearSeq[Double]],rate:Double=1.0,fact:ExpFactory=DefaultExpFactory) extends StdModel{
+class BasicLikelihoodModel(piValues:IndexedSeq[Double],s:LinearSeq[LinearSeq[Double]],rate:Double=1.0,fact:ExpFactory=DefaultExpFactory,override val subModelIndex:Int=0) extends StdModel{
   val mat = s.sToQ(piValues,rate)
   def pi(n:Node) = piValues
+  val pi = piValues
   val exp=DefaultExpFactory.apply(mat)
+  
 }
 
 class GammaModel(piValues:IndexedSeq[Double],s:LinearSeq[LinearSeq[Double]],alpha:Double,numCat:Int) extends StdMixtureModel{
   lazy val gamma = new Gamma(numCat).apply(alpha)
-  val models:Seq[StdModel] = gamma.map{new BasicLikelihoodModel(piValues,s,_)}
+  val models:Seq[StdModel] = gamma.zipWithIndex.map{t=>new BasicLikelihoodModel(piValues,s,t._1,subModelIndex=t._2)}
   val priors = Vector.fill(numCat)(1.0/numCat)
 }
 
